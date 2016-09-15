98ec1e3f7bf7564636701f3b9567b05da968fd99
Cory Kaufman-Schofield <cory@corykaufman.com>
2016-09-15 16:17:53 -0400

    Use prepend instead of include

--- lib/endeavour/core_ext.rb
+++ lib/endeavour/core_ext.rb
@@ -4,8 +4,8 @@ require 'endeavour/core_ext/nil_class/try'
 class Endeavour
   def self.hook!
     return if defined?(@try_removed)
-    Object.include CoreExt::Object::Try
-    NilClass.include CoreExt::NilClass::Try
+    Object.prepend CoreExt::Object::Try
+    NilClass.prepend CoreExt::NilClass::Try
   end
 
   def self.remove_hook!
