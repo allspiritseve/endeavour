08ac78eedc67394c81bf8501c0a66bbee44a37bf
Cory Kaufman-Schofield <cory@corykaufman.com>
2016-10-21 15:55:43 -0400

    Add a test that uses a block

--- test/endeavour_test.rb
+++ test/endeavour_test.rb
@@ -1,21 +1,47 @@
 require 'test_helper'
+require 'ostruct'
 
 Endeavour.hook!
 
 class EndeavourTest < Minitest::Test
-  def test_call
+  def test_call_with_object
+    color = OpenStruct.new(name: 'Red')
+    assert_equal 'Red', color.try(:name)
+
+    color = nil
+    assert_equal nil, color.try(:name)
+  end
+
+  def test_call_with_hash
     colors = { red: :rojo }
     assert_equal :rojo, colors.try(:[], :red)
+
+    colors = nil
+    assert_equal nil, colors.try(:[], :red)
   end
 
-  def test_delegate
+  def test_delegate_with_object
+    color = OpenStruct.new(name: 'Red')
+    assert_equal 'Red', color.try.name
+
+    color = nil
+    assert_equal nil, color.try.name
+  end
+
+  def test_delegate_with_hash
     colors = { red: :rojo }
     assert_equal :rojo, colors.try[:red]
-  end
 
-  def test_delegate_with_nil
     colors = nil
     assert_equal nil, colors.try[:red]
+  end
+
+  def test_delegate_block
+    numbers = 1.upto(5)
+    assert_equal [1, 4, 9, 16, 25], numbers.try.map { |n| n * n }
+
+    numbers = nil
+    assert_equal nil, numbers.try.map { |n| n * n }
   end
 
   def test_remove_hook
